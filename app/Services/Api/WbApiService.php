15e36e44f24f5be1f455b66baea0838505538fd9
51mpleDev <firdavs02shukurov@gmail.com>
2026-04-15 20:37:37 +0500

    update api service

--- app/Services/Api/WbApiService.php
+++ app/Services/Api/WbApiService.php
@@ -15,10 +15,37 @@ class WbApiService
         $this->apiKey = config('services.wb.key');
     }
 
-    public function get(string $endpoint, array $params = [])
+    public function get(string $endpoint, array $params = [], $token = null)
     {
         $params['key'] = $this->apiKey;
 
-        return Http::get($this->host . $endpoint, $params)->json();
+        $request = Http::retry(5, 2000);
+
+        if ($token) {
+            $request = $this->applyToken($request, $token);
+        }
+        return $request->get($this->host . $endpoint, $params)->json();
+    }
+
+    private function applyToken($request, $token)
+    {
+        $type = $token->tokenType->name;
+
+        return match ($type) {
+            'bearer' => $request->withHeaders([
+                'Authorization' => 'Bearer ' . $token->value
+            ]),
+
+            'api_key' => $request->withQueryParameters([
+                'key' => $token->value
+            ]),
+
+            'basic' => $request->withBasicAuth(
+                $token->value['login'],
+                $token->value['password']
+            ),
+
+            default => $request,
+        };
     }
 }
